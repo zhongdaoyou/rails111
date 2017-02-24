bd8cee601c72af1559a337bc6b33fc84fb2689ce
zhongdaoyou <18942371859@163.com>
2017-02-24 19:59:41 +0800

    join or quit group

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,8 +2,8 @@ class Group < ApplicationRecord
   belongs_to :user
   has_many :posts
   validates :title, presence: true
-
-  has_many :group_relationship
+  
+  has_many :group_relationships
   has_many :members, through: :group_relationships, source: :user
 
 end
